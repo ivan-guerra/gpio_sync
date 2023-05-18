941a331eef5644d46b0f7c4ad5d351f34917e229
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-05-17 23:25:53 -0700

    Implement a GPIO pin control utility class.
    The GPIO control utility class is modified version of Derek Molloy's
    GPIO control code found in the book "Exploring BeagleBone". The original
    source can be found here: https://github.com/derekmolloy/exploringBB/tree/version2/library/gpio

---
+++ include/gpio/gpio.hpp
@@ -0,0 +1,137 @@
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include <fstream>
+#include <string>
+
+namespace gsync {
+
+/**
+ * GPIO pin control utility.
+ *
+ * This source code is a modified version of Derek Molloy's GPIO control code
+ * from the book "Exploring BeagleBone".
+ */
+class Gpio {
+   public:
+    /** Pin input/output settings. */
+    enum class Direction {
+        kInput,
+        kOutput,
+    };
+
+    /** Pin high/low values. */
+    enum Value {
+        kLow = 0,
+        kHigh = 1,
+    };
+
+    /** Pin edge types. */
+    enum class Edge {
+        kNone,
+        kRising,
+        kFalling,
+        kBoth,
+    };
+
+    /**
+     * Construct a GPIO pin controller for the pin with the parameter number.
+     *
+     * The pin number passed to the constructor is the internal GPIO number.
+     * As an example, header label GPIOP1_17 translates to GPIO number (32 * 1)
+     * + 17 = 49.
+     *
+     * @param [in] number Export GPIO number.
+     */
+    explicit Gpio(int number);
+
+    /**
+     * GPIO pin controller destructor.
+     *
+     * The destructor will unexport the pin number given at construction.
+     */
+    ~Gpio() { UnexportGpio(); }
+
+    /* The following constructors/assignment overloads are deleted because the
+     * class uses a stream object that does not support copy/move. */
+    Gpio() = delete;
+    Gpio(const Gpio&) = delete;
+    Gpio& operator=(const Gpio&) = delete;
+    Gpio(Gpio&&) = delete;
+    Gpio& operator=(Gpio&&) = delete;
+
+    /** Return the GPIO number exported to /sys/class/gpio */
+    int SysFsNumber() const { return number_; }
+
+    /** Return true if the GPIO direction is set to the parameter direction. */
+    bool Dir(Direction direction) const;
+
+    /** Return the current in/out direction of the GPIO. */
+    Direction Dir() const;
+
+    /** Return true if the GPIO has the parameter value. */
+    bool Val(Value value) const;
+
+    /** Return the current low/high value of the GPIO. */
+    Value Val() const;
+
+    /**
+     * Toggle the GPIO value
+     *
+     * This method has the side effect of setting the GPIO to be an output pin.
+     */
+    void ToggleOutput() const;
+
+    /**
+     * Set or clear the active_low setting of the GPIO
+     *
+     * @param is_low [in] If set to true, the GPIO active_low property will be
+     * set, otherwise, the active_low property will be cleared.
+     */
+    bool SetActiveLow(bool is_low = true) const;
+
+    /** Return true if the GPIO is set active high. */
+    bool SetActiveHigh() const { return SetActiveLow(false); }
+
+    /* The original source of this code claims a ~20x speedup when writing GPIO
+     * values using stream operations versus using a write() sys call. */
+
+    /** Open an output stream to the GPIO's value file. */
+    void StreamOpen() { stream_.open((path_ + "value").c_str()); }
+
+    /** Write the parameter value to the GPIO value file. */
+    void StreamWrite(Value value) { stream_ << value << std::flush; }
+
+    /** Close an output stream previously opened with a call to StreamOpen(). */
+    void StreamClose() { stream_.close(); }
+
+    /** Return true if the value in the GPIO's edge file is set to edge. */
+    bool EdgeType(Gpio::Edge edge) const;
+
+    /** Return the current edge setting of the GPIO. */
+    Gpio::Edge EdgeType() const;
+
+    /**
+     * Block indefinitely until an edge trigger event is detected.
+     *
+     * This method has the side effect of setting the GPIO to be an input pin.
+     *
+     * @return True if an event is detected, false otherwise.
+     */
+    bool WaitForEdge();
+
+   private:
+    static const std::string kGpioPathPrefix;
+
+    bool ExportGpio() const;
+    bool UnexportGpio() const;
+
+    int number_;       /**< The GPIO number of the object. */
+    std::string name_; /**< The name of the GPIO (e.g., gpio50) */
+    std::string path_; /**< Path to the GPIO (e.g., /sys/class/gpio/gpio50) */
+    std::ofstream stream_; /**< Stream object used to write GPIO values. */
+};
+
+}  // namespace gsync
+
+#endif
---
+++ src/gpio/gpio.cc
@@ -0,0 +1,184 @@
+#include "gpio/gpio.hpp"
+
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+
+#include <cstdio>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+namespace gsync {
+
+const std::string Gpio::kGpioPathPrefix("/sys/class/gpio/");
+
+static bool Write(const std::string path, const std::string filename,
+                  const std::string value) {
+    std::ofstream fs((path + filename).c_str());
+    if (!fs) {
+        perror("write failed to open file ");
+        return false;
+    }
+    fs << value;
+    return true;
+}
+
+static bool Write(const std::string& path, const std::string& filename,
+                  int value) {
+    std::stringstream ss;
+    ss << value;
+    return Write(path, filename, ss.str());
+}
+
+static std::string Read(const std::string& path, const std::string& filename) {
+    std::ifstream fs((path + filename).c_str());
+    if (!fs) {
+        perror("read failed to open file");
+        return "";
+    }
+    std::string input("");
+    std::getline(fs, input);
+    return input;
+}
+
+bool Gpio::ExportGpio() const {
+    return Write(kGpioPathPrefix, "export", number_);
+}
+
+bool Gpio::UnexportGpio() const {
+    return Write(kGpioPathPrefix, "unexport", number_);
+}
+
+Gpio::Gpio(int number)
+    : number_(number),
+      name_("gpio" + std::to_string(number_)),
+      path_(kGpioPathPrefix + name_ + "/") {
+    ExportGpio();
+    static const int kSysFsSetupDelayUsec = 250000;
+    usleep(kSysFsSetupDelayUsec);
+}
+
+bool Gpio::Dir(Direction dir) const {
+    switch (dir) {
+        case Direction::kInput:
+            return Write(path_, "direction", "in");
+        case Direction::kOutput:
+            return Write(path_, "direction", "out");
+        default:
+            return false;
+    }
+}
+
+Gpio::Direction Gpio::Dir() const {
+    std::string input = Read(path_, "direction");
+    return (input == "in") ? Direction::kInput : Direction::kOutput;
+}
+
+bool Gpio::Val(Value value) const {
+    switch (value) {
+        case kHigh:
+            return Write(path_, "value", "1");
+        case kLow:
+            return Write(path_, "value", "0");
+        default:
+            return false;
+    }
+}
+
+Gpio::Value Gpio::Val() const {
+    std::string input = Read(path_, "value");
+    return (input == "0") ? Value::kLow : Value::kHigh;
+}
+
+void Gpio::ToggleOutput() const {
+    Dir(Direction::kOutput);
+    switch (Val()) {
+        case Value::kLow:
+            Val(Value::kHigh);
+            break;
+        case Value::kHigh:
+            Val(Value::kLow);
+            break;
+    }
+}
+
+bool Gpio::SetActiveLow(bool is_low) const {
+    if (is_low) {
+        return Write(path_, "active_low", "1");
+    } else {
+        return Write(path_, "active_low", "0");
+    }
+}
+
+bool Gpio::EdgeType(Edge edge) const {
+    switch (edge) {
+        case Edge::kNone:
+            return Write(path_, "edge", "none");
+        case Edge::kRising:
+            return Write(path_, "edge", "rising");
+        case Edge::kFalling:
+            return Write(path_, "edge", "falling");
+        case Edge::kBoth:
+            return Write(path_, "edge", "both");
+        default:
+            return false;
+    }
+}
+
+Gpio::Edge Gpio::EdgeType() const {
+    std::string input = Read(path_, "edge");
+    if (input == "rising") {
+        return Edge::kRising;
+    } else if (input == "falling") {
+        return Edge::kFalling;
+    } else if (input == "both") {
+        return Edge::kBoth;
+    } else {
+        return Edge::kNone;
+    }
+}
+
+bool Gpio::WaitForEdge() {
+    /* We have to set the pin to be an input pin order to poll it. */
+    Dir(Direction::kInput);
+
+    int epollfd = epoll_create(1);
+    if (-1 == epollfd) {
+        perror("failed to create epollfd");
+        return false;
+    }
+
+    int fd = open((path_ + "value").c_str(), O_RDONLY | O_NONBLOCK);
+    if (-1 == fd) {
+        perror("failed to open file");
+        return false;
+    }
+
+    struct epoll_event ev;
+    ev.events = EPOLLIN | EPOLLET | EPOLLPRI;
+    ev.data.fd = fd;
+
+    /* register the file descriptor on the epoll instance. */
+    if (-1 == epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev)) {
+        perror("failed to add control interface");
+        return false;
+    }
+
+    int events = 0;
+    bool success = true;
+    while (events <= 1) { /* we ignore the first event */
+        int rc = epoll_wait(epollfd, &ev, 1, -1);
+        if (-1 == rc) {
+            perror("poll wait fail");
+            success = false;
+            break;
+        }
+        events++;
+    }
+    close(fd);
+
+    return success;
+}
+
+}  // namespace gsync
