26f92c60b0c488330e9a1a769bb905a3211edd33
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-05-18 23:09:07 -0700

    Implement a driver that configures a GPIO and blocks for an event.

--- src/gtimer/gtimer.cc
+++ src/gtimer/gtimer.cc
@@ -1,7 +1,25 @@
 #include <iostream>
 
+#include "gpio/gpio.hpp"
+
 int main() {
-    std::cout << "Hello, world from the Timer" << std::endl;
+    const int kNumber = 115;
+    gsync::Gpio gpio(kNumber);
+
+    std::cout << "GPIO Number: " << gpio.SysFsNumber() << std::endl;
+    std::cout << "Default Direction: "
+              << ((gsync::Gpio::Direction::kInput == gpio.Dir()) ? "in" : "out")
+              << std::endl;
+    std::cout << "Default Value: "
+              << ((gsync::Gpio::Value::kLow == gpio.Val()) ? "0" : "1")
+              << std::endl;
+
+    gpio.EdgeType(gsync::Gpio::Edge::kRising);
+    std::cout << "Set Edge Type to Rising" << std::endl;
+
+    std::cout << "Waiting for Trigger Event..." << std::endl;
+    gpio.WaitForEdge();
+    std::cout << "Detected Event!" << std::endl;
 
     return 0;
 }
