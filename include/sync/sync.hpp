236a22d7390923aa241810b903b1d386cd8bfbb2
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-05-23 09:34:35 -0700

    Use the Kuramoto Model to compute wakeup times that bring the machines closer to synchronization.

---
+++ include/sync/sync.hpp
@@ -0,0 +1,71 @@
+#ifndef SYNC_H_
+#define SYNC_H_
+
+#include <time.h>
+
+namespace gsync {
+
+class KuramotoSync {
+   public:
+    static const int kNumParticipants = 2; /**< Machines in the sync loop. */
+
+    /**
+     * Construct a Kuramoto sync object with the specified frequency and
+     * coupling constant.
+     *
+     * @param[in] frequency Frequency in Hertz at which this task runs.
+     * @param[in] coupling_constant The coupling constant, K, in the Kuramoto
+     * Model.
+     *
+     * @throws std::runtime_error
+     */
+    KuramotoSync(int frequency, double coupling_constant);
+
+    /* The default copy/move constructors and assignment methods will work for
+     * objects of this class. */
+    KuramotoSync() = delete;
+    ~KuramotoSync() = default;
+    KuramotoSync(const KuramotoSync&) = default;
+    KuramotoSync& operator=(const KuramotoSync&) = default;
+    KuramotoSync(KuramotoSync&&) = default;
+    KuramotoSync& operator=(KuramotoSync&&) = default;
+
+    /** Return the base frequency in Hertz. */
+    int Frequency() const { return frequency_; }
+
+    /** Return the coupling constant. */
+    double CouplingConstant() const { return coupling_constant_; }
+
+    /**
+     * Run the Kuramoto algorithm to compute this participant's new wakeup time.
+     *
+     * @param[in] expected_wakeup The time when this participant expected to
+     * wakeup in its current cycle.
+     * @param[in] actual_wakeup The time when this participant actually wokeup
+     * to begin its current cycle.
+     * @param[in] peer_wakeup The last wakeup time reported by this
+     * participant's peer.
+     *
+     * @returns The new wakeup time for this participant which would bring him
+     * closer to or keep him in sync with his peer.
+     */
+    timespec ComputeNewWakeup(const timespec& expected_wakeup,
+                              const timespec& actual_wakeup,
+                              const timespec& peer_wakeup) const;
+
+   private:
+    static constexpr double kSecToNano = 1e9;
+    static constexpr double kPi = 3.141592653589793;
+
+    double ToNano(const timespec& ts) const;
+    double NanoToRad(double ns) const;
+    double RadToNano(double rad) const;
+    void NormalizeTime(timespec& ts) const;
+
+    int frequency_;
+    double coupling_constant_;
+};
+
+}  // namespace gsync
+
+#endif
---
+++ src/sync/sync.cc
@@ -0,0 +1,74 @@
+#include "sync/sync.hpp"
+
+#include <cmath>
+#include <cstdint>
+#include <stdexcept>
+
+namespace gsync {
+
+double KuramotoSync::ToNano(const timespec& ts) const {
+    double sec_to_nano = static_cast<double>(ts.tv_sec) * kSecToNano;
+    return (sec_to_nano + static_cast<double>(ts.tv_nsec));
+}
+
+double KuramotoSync::NanoToRad(double ns) const {
+    static const double kConvFactor = (2 * kPi * frequency_) / kSecToNano;
+    return (kConvFactor * ns);
+}
+
+double KuramotoSync::RadToNano(double rad) const {
+    static const double kConvFactor = (kSecToNano / (2 * kPi * frequency_));
+    return (kConvFactor * rad);
+}
+
+void KuramotoSync::NormalizeTime(timespec& ts) const {
+    static const int kNanoSecPerSec = 1e9;
+    while (ts.tv_nsec >= kNanoSecPerSec) {
+        ts.tv_sec++;
+        ts.tv_nsec -= kNanoSecPerSec;
+    }
+}
+
+KuramotoSync::KuramotoSync(int frequency, double coupling_constant)
+    : frequency_(frequency), coupling_constant_(coupling_constant) {
+    if (frequency_ <= 0) {
+        throw std::runtime_error("frequency must be greater than 0");
+    }
+    if (coupling_constant_ <= 0.0) {
+        throw std::runtime_error("coupling constant must be greater than 0");
+    }
+}
+
+timespec KuramotoSync::ComputeNewWakeup(const timespec& expected_wakeup,
+                                        const timespec& actual_wakeup,
+                                        const timespec& peer_wakeup) const {
+    double expected_wakeup_ns = ToNano(expected_wakeup);
+    double actual_wakeup_ns = ToNano(actual_wakeup);
+    double peer_wakeup_ns = ToNano(peer_wakeup);
+
+    /* Compute the variables of the Kuramoto Model for the current run. */
+    double omega_i = (1.0 / frequency_) * kSecToNano;
+    double dt_i = expected_wakeup_ns - actual_wakeup_ns;
+    double dtheta_i = NanoToRad(dt_i);
+    double dt_j = expected_wakeup_ns - peer_wakeup_ns;
+    double dtheta_j = NanoToRad(dt_j);
+
+    /* Straightforward implementation of the common form of the Kuramoto Model
+     * as seen here https://en.wikipedia.org/wiki/Kuramoto_model */
+    double dtheta_dt =
+        omega_i +
+        ((coupling_constant_ / static_cast<double>(kNumParticipants)) *
+         std::sin(dtheta_j - dtheta_i));
+
+    /* The new wakeup time is an offset from the actual wakeup time. */
+    timespec new_wakeup = {
+        .tv_sec = actual_wakeup.tv_sec,
+        .tv_nsec =
+            actual_wakeup.tv_nsec + static_cast<long>(RadToNano(dtheta_dt)),
+    };
+    NormalizeTime(new_wakeup);
+
+    return new_wakeup;
+}  // namespace gsync
+
+}  // namespace gsync
